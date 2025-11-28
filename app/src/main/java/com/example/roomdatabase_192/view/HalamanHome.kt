1e509361a6bbfaef51206b53ed581318d1864180
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 14:13:28 +0700

    Menambahkan Scaffold pada HomeScreen yang memuat SiswaTopAppBar tanpa navigasi balik, FloatingActionButton untuk menavigasi ke entry siswa

--- app/src/main/java/com/example/roomdatabase_192/view/HalamanHome.kt
+++ app/src/main/java/com/example/roomdatabase_192/view/HalamanHome.kt
@@ -50,3 +50,36 @@ fun HomeScreen(
 ){
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        },
+    ){
+            innerPadding ->
+        val uiStateSiswa by viewModel.homeViewModel.collectAsState()
+        BodyHome(
+            itemSiswa = uiStateSiswa.listSiswa,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+
