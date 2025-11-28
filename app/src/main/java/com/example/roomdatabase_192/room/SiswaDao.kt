4cbb969d6854151a7e58777ef7ac555db05b733b
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 13:47:11 +0700

    menambahkan fungsi untuk menyimpan data siswa

--- app/src/main/java/com/example/roomdatabase_192/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase_192/room/SiswaDao.kt
@@ -13,3 +13,6 @@ interface SiswaDao {
     @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa() : Flow<List<Siswa>>
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
