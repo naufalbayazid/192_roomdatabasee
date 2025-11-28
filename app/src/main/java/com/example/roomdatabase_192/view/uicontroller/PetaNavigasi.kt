5d9414d325782646a8d303394f06d92a52094c1f
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 13:52:25 +0700

    menambahkan fungsi composable untuk mengatur navigasi

---
+++ app/src/main/java/com/example/roomdatabase_192/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,21 @@
+package com.example.roomdatabase_192.view.uicontroller
+
+
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.roomdatabase_192.view.EntrySiswaScreen
+import com.example.roomdatabase_192.view.HomeScreen
+import com.example.roomdatabase_192.view.route.DestinasiEntry
+import com.example.roomdatabase_192.view.route.DestinasiHome
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
