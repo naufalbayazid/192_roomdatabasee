dd6fd13cea96753a67a4954d28cdd689f926d794
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 14:21:13 +0700

    Menambahkan fungsi ekstensi toUiStateSiswa() dan toDetailSiswa() untuk memudahkan konversi objek Siswa menjadi UIStateSiswa dan DetailSiswa

--- app/src/main/java/com/example/roomdatabase_192/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_192/viewmodel/EntryViewModel.kt
@@ -51,3 +51,21 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
