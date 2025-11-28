b287a367ba7f794acbbdf1b67b94a4a880031fbb
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 14:02:56 +0700

    Menambahkan EntrySiswaScreen composable yang menerima navigateBack lambda dan EntryViewModel untuk menangani logika input data siswa.

--- app/src/main/java/com/example/roomdatabase_192/view/HalamanEntry.kt
+++ app/src/main/java/com/example/roomdatabase_192/view/HalamanEntry.kt
@@ -1,2 +1,45 @@
 package com.example.roomdatabase_192.view
 
+
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomdatabase_192.R
+import com.example.roomdatabase_192.view.route.DestinasiEntry
+import com.example.roomdatabase_192.viewmodel.DetailSiswa
+import com.example.roomdatabase_192.viewmodel.EntryViewModel
+import com.example.roomdatabase_192.viewmodel.UIStateSiswa
+import com.example.roomdatabase_192.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
