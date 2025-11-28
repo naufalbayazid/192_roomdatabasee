4cb43df5b67460bc44d6a94eed20974c3d15e3c1
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 13:25:59 +0700

    membuat Dao RepositoriSiswa.kt

--- app/src/main/java/com/example/roomdatabase_192/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase_192/repositori/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.roomdatabase_192.repositori
 
+
+
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase_192.room.DatabaseSiswa
+
+
+
+interface ContainerApp {
+    val repositorySiswa : RepositoriSiswa
+}
+
--- app/src/main/java/com/example/roomdatabase_192/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_192/repositori/RepositoriSiswa.kt
@@ -1,2 +1,30 @@
 package com.example.roomdatabase_192.repositori
 
+
+
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase_192.room.DatabaseSiswa
+
+
+
+interface ContainerApp {
+    val repositorySiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp{
+    override val repositorySiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context=this)
+    }
+}
