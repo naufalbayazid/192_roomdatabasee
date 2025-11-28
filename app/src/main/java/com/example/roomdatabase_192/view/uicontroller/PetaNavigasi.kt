f5cf23cb053ec2bef71f5b0af2307b82d427ce0f
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 13:54:38 +0700

    Menambahkan konfigurasi NavHost yang memuat rute HomeScreen dan EntrySiswaScreen, termasuk navigasi ke entry dan aksi popBackStack.

--- app/src/main/java/com/example/roomdatabase_192/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase_192/view/uicontroller/PetaNavigasi.kt
@@ -24,4 +24,17 @@ fun SiswaApp(navController: NavHostController = rememberNavController(), modifie
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-)
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+
+    }
+}
