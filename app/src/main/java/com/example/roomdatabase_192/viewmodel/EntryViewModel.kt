8255a4f53405c4f018b52e1dca54120ada43a775
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-28 14:18:50 +0700

    Menambahkan EntryViewModel yang menyimpan uiStateSiswa sebagai mutableStateOf dan fungsi validasiInput

--- app/src/main/java/com/example/roomdatabase_192/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_192/viewmodel/EntryViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.roomdatabase_192.viewmodel
 
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase_192.repositori.RepositoriSiswa
+import com.example.roomdatabase_192.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa
+            .detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
