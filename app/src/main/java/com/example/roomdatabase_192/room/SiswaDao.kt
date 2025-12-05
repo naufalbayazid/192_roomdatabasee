b70e091e89c6f21b357c42ad161672ef619eb41b
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-12-05 13:39:22 +0700

    menambahkan 3 di SiswaDao Query, Update,Dan Delete

--- app/src/main/java/com/example/roomdatabase_192/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase_192/room/SiswaDao.kt
@@ -3,16 +3,28 @@ package com.example.roomdatabase_192.room
 
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
-    fun getAllSiswa() : Flow<List<Siswa>>
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    //Edit : tambah fungsi berikut
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
