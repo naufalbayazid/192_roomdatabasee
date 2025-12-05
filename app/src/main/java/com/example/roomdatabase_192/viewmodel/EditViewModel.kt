effbc4885dddf599aaa5161a2a202ae18af47880
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-12-05 13:45:21 +0700

    menambahkan class baru yang berisikan repository siswa dan menambahkan integer gaboleh kosong isinya

---
+++ app/src/main/java/com/example/roomdatabase_192/viewmodel/EditViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.roomdatabase_192.viewmodel
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase_192.repositori.RepositoriSiswa
+import com.example.roomdatabase_192.view.route.DestinasiDetailSiswa
+import com.example.roomdatabase_192.view.route.DestinasiEditSiswa
+
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiEditSiswa.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoriSiswa.getSiswaStream(idSiswa)
+                .filterNotNull()
+                .first()
+                .toUiStateSiswa(true)
+        }
+    }
+
