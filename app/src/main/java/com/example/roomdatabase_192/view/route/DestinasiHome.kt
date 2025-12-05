336ef573f22e1869cf7d764e120c0e7512eddf44
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-12-05 13:49:36 +0700

    penambahan fungsi baru di DestinasiHome.kt

---
+++ app/src/main/java/com/example/roomdatabase_192/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.roomdatabase_192.view.route
+
+import com.example.roomdatabase_192.R
+
+object DestinasiHome: DestinasiNavigasi {
+
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
