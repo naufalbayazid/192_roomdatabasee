5ae3680240e362b3c0dc3ba9f7dcef403d3a756b
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-12-05 13:42:58 +0700

    menambahkan codingan, akan tetapi masih error

--- app/src/main/java/com/example/roomdatabase_192/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_192/viewmodel/provider/PenyediaViewModel.kt
@@ -2,13 +2,16 @@ package com.example.roomdatabase_192.viewmodel.provider
 
 
 
-
-
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase_192.repositori.AplikasiSiswa
+import com.example.roomdatabase_192.viewmodel.DetailViewModel
+import com.example.roomdatabase_192.viewmodel.EditViewModel
+import com.example.roomdatabase_192.viewmodel.EntryViewModel
+import com.example.roomdatabase_192.viewmodel.HomeViewModel
 import com.example.roomdatabase_192.viewmodel.EntryViewModel
 import com.example.roomdatabase_192.viewmodel.HomeViewModel
 
@@ -22,7 +25,17 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositorySiswa)
         }
+
+        initializer {
+            DetailViewModel(savedStateHandle = this.createSavedStateHandle(),
+                repositoriSiswa = aplikasiSiswa().container.repositorySiswa)
+        }
+
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositorySiswa)
+        }
     }
+
 }
 
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
